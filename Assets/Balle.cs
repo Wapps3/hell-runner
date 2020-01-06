fef822f5ba715506349c2d7a5463f7e1a7fbac23
Duvignau Thomas <thomaslaforce@yahoo.fr>
2020-01-06 20:15:40 +0100

    Add a new ennemy and his projectile

---
+++ Assets/Balle.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Balle : MonoBehaviour
+{
+    public float speed = 10.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        gameObject.transform.Translate(Time.deltaTime * speed,0,0); 
+    }
+
+    void OnTriggerEnter(Collider c)
+    {
+        if (c.tag == "Player")
+        {
+            c.GetComponent<Personnage>().Hit();
+        }
+
+        Destroy(gameObject);
+    }
+}
---
+++ Assets/Scripts/Game/Weapon.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Weapon : MonoBehaviour
+{
+    public float time;
+
+    private Transform firePoint;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+       firePoint = gameObject.transform.Find("FirePoint").transform;
+    }
+
+    public void Shoot()
+    {
+
+    }
+
+    IEnumerator TimeBetweenShoot()
+    {
+        Shoot();
+        yield return new WaitForSeconds(time);
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        StartCoroutine(TimeBetweenShoot());
+    }
+}
