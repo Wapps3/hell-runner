d87a78d50d8658807dfaa72bdd167a77907350f8
Sydney <sydney.delobel@gmail.com>
2020-01-07 10:14:22 +0100

    Update Level + Debut Game Over

---
+++ Assets/Scripts/Scenes/SceneSwitcher.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneSwitcher : MonoBehaviour
+{
+    public void GotoLevelScene()
+    {
+        SceneManager.LoadScene("Level");
+    }
+
+    public void GotoMenuScene()
+    {
+        SceneManager.LoadScene("LoginScreen");
+    }
+}
